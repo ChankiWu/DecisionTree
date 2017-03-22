2252f5b18195b773f33ed0e5703001ea779f5f50
ChankiWu <254152970@qq.com>
2017-03-22 20:53:28 +0800

    该函数使用分类名称的列表，然后创建键值为classList中唯一值的数据字典，字典对象存储了classList中每个类标签出现的概率，最后利用operator操作键值排序字典，并返回出现次数最多的分类名称。

--- trees.py
+++ trees.py
@@ -56,3 +56,10 @@ def chooseBestFeatureToSplit(dataSet):
 
     return bestFeature
 
+def majorityCnt(classList):
+    classCount = {}
+    for vote in classList:
+        if vote not in classCount.keys(): classCount[vote] = 0
+        classCount[vote] += 1
+    sortedClassCount = sorted(classCount.iteritems(),key=operator.itemgetter(1),reverse=True)
+    return sortedClassCount[0][0]
